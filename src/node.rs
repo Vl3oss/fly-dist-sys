6567473325e0ed38570928673995839c81bb6b63
varitthorn boss <varitthorn@protonmail.com>
2023-04-17 23:38:06 +0700

    refactor: add read loop

--- src/node.rs
+++ src/node.rs
@@ -42,6 +42,16 @@ where
         msg_raw
     }
 
+    pub fn read_loop<H>(self: &mut Self, on_msg: H) -> !
+    where
+        H: Fn(&mut Self, String) -> (),
+    {
+        loop {
+            let msg_str = self.read();
+            on_msg(self, msg_str);
+        }
+    }
+
     pub fn initialize(self: &mut Self, node_id: NodeId, node_ids: Vec<NodeId>) -> () {
         self.node_id = Some(node_id);
         self.node_ids = node_ids;
@@ -96,17 +106,15 @@ where
     }
 
     pub fn main_loop(self: &mut Self) -> ! {
-        loop {
-            let req_str = self.read();
-
-            let res_msg = match self.node_state {
-                NodeState::Uninitialized => self.try_handle_init(&req_str),
-                NodeState::Initialized => self.handle(&req_str),
+        self.read_loop(|node, req_str| {
+            let res_msg = match node.node_state {
+                NodeState::Uninitialized => node.try_handle_init(&req_str),
+                NodeState::Initialized { .. } => node.handle(&req_str),
             };
 
             if let Some(msg) = res_msg {
-                self.send(msg);
+                node.send(msg);
             }
-        }
+        })
     }
 }
